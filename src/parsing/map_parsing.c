1326a0dd24be787021dfa4795dad74b8e74ec64a
jovica pc fixe <jovicadufour@gmail.com>
2024-04-21 17:15:18 +0200

    fix east

--- src/parsing/map_parsing.c
+++ src/parsing/map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 16:49:26 by jdufour           #+#    #+#             */
-/*   Updated: 2024/04/19 18:36:54 by taospa           ###   ########.fr       */
+/*   Updated: 2024/04/21 17:14:45 by jdufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	get_player_pos(t_data *data, char c, int y, int x)
 	else if (c == 'S')
 		ch_plr_dir(data->player, (t_vect){0, 1}, (t_vect){(double) -0.66, 0.0});
 	else if (c == 'E')
-		ch_plr_dir(data->player, (t_vect){1, 0}, (t_vect){(double) 0.66, 0.0});
+		ch_plr_dir(data->player, (t_vect){1, 0}, (t_vect){(double) 0.0, 0.66});
 	else if (c == 'W')
 		ch_plr_dir(data->player, (t_vect){-1, 0}, (t_vect){0.0, -0.66});
 	data->player->pos = (t_vect){(double)x, (double)y};
